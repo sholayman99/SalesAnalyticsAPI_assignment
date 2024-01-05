65d19d4dbc8c158016c765bd67acf7def540bd9e
Md Sholayman <mdsholayman998@gmail.com>
2024-01-05 21:31:18 +0600

    initialize the project

---
+++ src/routes/api.js
@@ -0,0 +1,11 @@
+/*
+* Author: Md. Sholayman
+* Description: This is the file that contains all the api end-point of the app
+* Date :  05 January 2024
+* */
+
+const express = require("express");
+const router = express.Router();
+
+
+module.exports = router;
